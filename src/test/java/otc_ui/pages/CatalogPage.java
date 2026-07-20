aefffbfd045cc4bfb91cd75f86c7abfc8aaf5d6a
Miray <mariya.erris@gmail.com>
2026-07-20 17:38:55 +0300

    Реализована замена Thread.sleep на явные ожидания Selenide, сбор названий и цен через коллекции, навигация по страницам, запись результатов в products.txt с проверками Assertions

--- src/test/java/otc_ui/pages/CatalogPage.java
+++ src/test/java/otc_ui/pages/CatalogPage.java
@@ -1,7 +1,15 @@
 package otc_ui.pages;
 
+import static com.codeborne.selenide.Condition.*;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class CatalogPage {
@@ -9,9 +17,14 @@ public class CatalogPage {
     private final SelenideElement searchInput = $("input[placeholder='Название товара']");
     private final SelenideElement regionSelectorButton = $("div[class*='SeoRegionSelector']");
     private final SelenideElement searchButton = $x("//span[text()='Найти']");
-        private final SelenideElement checkboxKrasnodar = $x("//*[text()='г. Краснодар']");
+    private final SelenideElement checkboxKrasnodar = $x("//*[text()='г. Краснодар']");
     private final SelenideElement checkboxMoscow = $x("//*[text()='г. Москва']");
     private final SelenideElement buttonSubmitRegion = $x("//span[text()='Применить']");
+
+    private final ElementsCollection productNames = $$("a[itemprop='name']");
+    private final ElementsCollection productPrices = $$("[itemprop='price']");
+
+    private final SelenideElement pageTwoButton = $x("//a[text()='2']");
 
     public CatalogPage openPage(String url) {
         Selenide.open(url);
@@ -20,6 +33,9 @@ public class CatalogPage {
 
     public CatalogPage enterSearchQuery(String query) {
         searchInput.setValue(query);
+
+        searchInput.shouldHave(value(query));
+
         return this;
     }
 
@@ -38,13 +54,37 @@ public class CatalogPage {
         return this;
     }
 
-    public CatalogPage applyRegion() {
+    public CatalogPage applyRegion(String expectedCity) {
         buttonSubmitRegion.click();
+
+        regionSelectorButton.shouldHave(text(expectedCity));
+
         return this;
     }
 
     public CatalogPage clickSearch() {
         searchButton.click();
+        return this;
+    }
+
+    public List<String> collectProductsData() {
+        List<String> parsedProducts = new ArrayList<>();
+
+        productNames.shouldHave(CollectionCondition.sizeGreaterThan(0));
+
+        for (int i = 0; i < productNames.size(); i++) {
+            String name = productNames.get(i).getText();
+            String rawPrice = productPrices.get(i).getText();
+
+            String cleanPrice = rawPrice.replaceAll("[^0-9,]", "");
+
+            parsedProducts.add(name + ", " + cleanPrice + " Руб/Штука");
+        }
+        return parsedProducts;
+    }
+
+    public CatalogPage goToSecondPage() {
+        pageTwoButton.scrollTo().click();
         return this;
     }
 }
--- src/test/java/otc_ui/test/SearchTest.java
+++ src/test/java/otc_ui/test/SearchTest.java
@@ -1,23 +1,51 @@
 package otc_ui.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import otc_ui.config.ConfigProvider;
 import otc_ui.core.BaseTest;
 import otc_ui.pages.CatalogPage;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchTest extends BaseTest {
 
     @Test
-    public void testSearchAndFilter() {
+    public void testSearchAndFilter() throws IOException {
         CatalogPage catalogPage = new CatalogPage();
 
         catalogPage.openPage(ConfigProvider.getBaseUrl())
                 .openRegionSelector()
                 .toggleKrasnodar()
                 .toggleMoscow()
-                .applyRegion()
+                .applyRegion(ConfigProvider.getSearchCity())
                 .enterSearchQuery(ConfigProvider.getSearchItem())
                 .clickSearch();
 
+        List<String> allProducts = new ArrayList<>(catalogPage.collectProductsData());
+
+        catalogPage.goToSecondPage();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        allProducts.addAll(catalogPage.collectProductsData());
+
+        Path filePath = Paths.get("target/products.txt");
+        Files.write(filePath, allProducts);
+
+        Assertions.assertTrue(Files.exists(filePath), "Файл products.txt не был создан!");
+        Assertions.assertFalse(allProducts.isEmpty(), "Список товаров пуст! Проверь локаторы сбора.");
+
+        System.out.println("Собрано товаров суммарно: " + allProducts.size());
+        System.out.println("Данные успешно сохранены в: " + filePath.toAbsolutePath());
     }
 }
